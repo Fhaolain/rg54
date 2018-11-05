d99cccb0e48da21993d86acbd043c3f378487503
ME-DEV-MASTER\Demo <jason.laws@wherescape.com>
2018-11-04 19:31:29 -0800

    Several changes to tidy things up

--- Format-Extprop.bat
+++ Format-Extprop.bat
@@ -1,5 +1,4 @@
 @echo off
 set currentDir=%~dp0
-Powershell -C "move-item ${env:currentDir}\snowflake.extprop ${env:currentDir}\snowflake.extprop.old"
-Powershell -C "get-content ${env:currentDir}\snowflake.extprop.old | convertfrom-JSON | convertto-JSON | set-content ${env:currentDir}\snowflake.extprop"
+Powershell -C "get-content ${env:currentDir}\snowflake.extprop | convertfrom-JSON | convertto-JSON | set-content ${env:currentDir}\snowflake.compare -force"
 pause
