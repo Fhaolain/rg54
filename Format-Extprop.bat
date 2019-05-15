2ffa9dd034e73a020b54a65c33f25bb9fd9f56f3
ME-DEV-MASTER\Demo <jason.laws@wherescape.com>
2019-05-14 19:38:24 -0700

    Tidy up of folder structure

--- Format-Extprop.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set currentDir=%~dp0
-Powershell -C "get-content ${env:currentDir}\snowflake.extprop | convertfrom-JSON | convertto-JSON | set-content ${env:currentDir}\snowflake.compare -force"
-pause
